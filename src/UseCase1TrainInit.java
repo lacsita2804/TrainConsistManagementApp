e71f81fd639659d6d6bc510ed69635caac41237a
K.A Lacsita <kalac@example.com>
2026-04-09 09:45:36 +0530

    UC1 Initialize Train Consist using ArrayList

---
+++ src/UseCase1TrainInit.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class UseCase1TrainInit {
+
+    public static void main(String[] args) {
+
+        // Welcome message
+        System.out.println("=== Train Consist Management App ===");
+
+        // Initialize empty train consist
+        List<String> train = new ArrayList<>();
+
+        // Display initial bogie count
+        System.out.println("Initial Bogie Count: " + train.size());
+    }
+}

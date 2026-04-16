6d346a9e24c399c6d27d57e59c01d9484cdf6867
K.A Lacsita <kalac@example.com>
2026-04-16 23:00:13 +0530

    UC17 Sorting using Arrays.sort()

---
+++ src/UseCase17SortBogieNames.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class UseCase17SortNames {
+    public static void main(String[] args) {
+        String[] names = {"Sleeper","AC Chair","First Class"};
+
+        Arrays.sort(names);
+
+        System.out.println(Arrays.toString(names));
+    }
+}
